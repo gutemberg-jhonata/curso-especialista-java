bf3cee17d33332997a2fb3422012ac3790d47455
Jhonata <gutemberg.jhonata@gmail.com>
2023-07-23 19:18:16 -0300

    resolvendo desafio

--- desafio-lambda-method-reference/src/Principal.java
+++ desafio-lambda-method-reference/src/Principal.java
@@ -1,5 +1,6 @@
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Principal {
 
@@ -10,15 +11,20 @@ public class Principal {
 		funcionarios.add(new Funcionario("Manoel", new BigDecimal("13000"), true));
 		funcionarios.add(new Funcionario("Sebastião", new BigDecimal("12000"), false));
 
-		// TODO remover funcionários inativos
-
-		// TODO ordenar funcionários pelo valor do salário
-
-		// TODO iterar e imprimir funcionários usando o método estático imprimir
+		funcionarios.removeIf(Funcionario::isInativo);
+		funcionarios.sort(Comparator.comparing(Funcionario::getSalario));
+		funcionarios.forEach(Principal::imprimir);
 	}
 
 	private static void imprimir(Funcionario funcionario) {
-		// TODO implementar a impressão do nome, salário e impostos (20%)
+		BigDecimal taxa = BigDecimal.valueOf(0.20);
+		BigDecimal impostos = funcionario.getSalario().multiply(taxa);
+		System.out.printf(
+			"Funcionario{nome='%s' salario=%.2f impostos=%.2f}%n", 
+			funcionario.getNome(),
+			funcionario.getSalario(),
+			impostos
+		);
 	}
 
 }
