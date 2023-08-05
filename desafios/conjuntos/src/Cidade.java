75578fb0f275b69949d135a666902c653dc025bf
Jhonata <gutemberg.jhonata@gmail.com>
2023-08-05 12:25:42 -0300

    resolvendo desafio

---
+++ desafios/conjuntos/src/Cidade.java
@@ -0,0 +1,73 @@
+public class Cidade implements Comparable<Cidade> {
+
+    private int codigoIbge;
+    private String nome;
+    private int totalHabitantes;
+
+    public Cidade(int codigoIbge, String nome, int totalHabitantes) {
+        this.codigoIbge = codigoIbge;
+        this.nome = nome;
+        this.totalHabitantes = totalHabitantes;
+    }
+
+    public int getCodigoIbge() {
+        return codigoIbge;
+    }
+
+    public void setCodigoIbge(int codigoIbge) {
+        this.codigoIbge = codigoIbge;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getTotalHabitantes() {
+        return totalHabitantes;
+    }
+
+    public void setTotalHabitantes(int totalHabitantes) {
+        this.totalHabitantes = totalHabitantes;
+    }
+
+    @Override
+    public String toString() {
+        return "Cidade{" +
+                "codigoIbge=" + codigoIbge +
+                ", nome='" + nome + '\'' +
+                ", totalHabitantes=" + totalHabitantes +
+                '}';
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + codigoIbge;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Cidade other = (Cidade) obj;
+        if (codigoIbge != other.codigoIbge)
+            return false;
+        return true;
+    }
+
+    @Override
+    public int compareTo(Cidade o) {
+        return Integer.compare(getCodigoIbge(), o.codigoIbge);
+    }
+
+}
