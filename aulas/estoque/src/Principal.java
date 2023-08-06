a2ed66fa175cb13facb8c81219a583fccdfc48ee
Jhonata <gutemberg.jhonata@gmail.com>
2023-08-06 08:52:26 -0300

    anyMatch, allMatch e noneMatch

--- aulas/estoque/src/Principal.java
+++ aulas/estoque/src/Principal.java
@@ -2,6 +2,7 @@ import com.algaworks.estoque.CadastroProduto;
 import com.algaworks.estoque.Produto;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 public class Principal {
@@ -15,11 +16,56 @@ public class Principal {
 //            System.out.println(produto);
 //        }
 
-        Stream<Produto> stream = produtos.stream();
-        stream.forEach(produto -> {
+        /*boolean temProdutoNoEstoque = produtos.stream()
+            .peek(System.out::println)
+            .anyMatch(Produto::temEstoque);
+        System.out.println(temProdutoNoEstoque);*/
+
+        /*boolean todosProdutosPossuemEstoque = produtos.stream()
+            .peek(System.out::println)
+            .allMatch(Produto::temEstoque);
+        System.out.println(todosProdutosPossuemEstoque);*/
+
+        boolean nenhumProdutoPossuiEstoque = produtos.stream()
+            .peek(System.out::println)
+            .noneMatch(Produto::temEstoque);
+        System.out.println(nenhumProdutoPossuiEstoque);
+
+        /*produtos.stream()
+            .peek(produto -> produto.setNome(produto.getNome().toUpperCase()))
+            .peek(System.out::println)
+            .filter(Produto::temEstoque)
+            .filter(Produto::isInativo)
+            .forEach(produto -> {
+                System.out.println("Ativando: " + produto);
+                produto.ativar();
+            });;*/
+
+        /*Optional<Produto> produtoOptional = produtos.stream()
+            .peek(System.out::println)
+            .filter(Produto::temEstoque)
+            .filter(Produto::isInativo)
+            .findFirst();
+
+        Produto produto = produtoOptional.orElseThrow(
+            () -> new RuntimeException("Produto não encontrado")
+        );
+
+        System.out.println("---------");
+        System.out.println(produto);*/
+
+        /*Stream<Produto> stream = produtos.stream();
+
+        Stream<Produto> streamComEstoque = stream
+            .filter(Produto::temEstoque);
+
+        Stream<Produto> streamComEstoqueInativo = streamComEstoque
+            .filter(Produto::isInativo);
+
+        streamComEstoqueInativo.forEach(produto -> {
             produto.ativar();
             System.out.println(produto);
-        });
+        });*/
     }
 
 }
