06d49075d1263c14d97368842edead93e5f08665
Jhonata <gutemberg.jhonata@gmail.com>
2023-08-07 11:01:01 -0300

    refatornado codigo principal 4

--- desafios/streams/src/Principal4.java
+++ desafios/streams/src/Principal4.java
@@ -10,12 +10,17 @@ public class Principal4 {
         var servicoDeVenda = new ServicoDeVenda();
         List<Venda> vendas = servicoDeVenda.obterTodas();
 
-        BigDecimal totalVendas = BigDecimal.ZERO;
+        BigDecimal totalVendas = vendas.stream()
+            .filter(Venda::isFechada)
+            .map(Venda::getValorTotal)
+            .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+        /*BigDecimal totalVendas = BigDecimal.ZERO;
         for (Venda venda : vendas) {
             if (venda.isFechada()) {
                 totalVendas = totalVendas.add(venda.getValorTotal());
             }
-        }
+        }*/
 
         System.out.println(totalVendas);
     }
