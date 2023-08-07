059cef1594611d7e03777385a0d618fc739bfa2d
Jhonata <gutemberg.jhonata@gmail.com>
2023-08-07 11:04:39 -0300

    refatornando codigo principal 5

--- desafios/streams/src/Principal5.java
+++ desafios/streams/src/Principal5.java
@@ -9,14 +9,20 @@ public class Principal5 {
         var servicoDeVenda = new ServicoDeVenda();
         List<Venda> vendas = servicoDeVenda.obterTodas();
 
-        int quantidadeItensVendidos = 0;
+        int quantidadeItensVendidos = vendas.stream()
+            .filter(Venda::isFechada)
+            .flatMap(venda -> venda.getItens().stream())
+            .map(Venda.Item::quantidade)
+            .reduce(0, Integer::sum);
+
+        /*int quantidadeItensVendidos = 0;
         for (Venda venda : vendas) {
             if (venda.isFechada()) {
                 for (Venda.Item item : venda.getItens()) {
                     quantidadeItensVendidos += item.quantidade();
                 }
             }
-        }
+        }*/
 
         System.out.println(quantidadeItensVendidos);
     }
